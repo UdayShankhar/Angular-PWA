c0113d46b93669dc49ebf22b41fb7be1f736651d
PickMyAd <pma-mac-01@PickMyAds-MacBook-Pro.local>
2023-08-25 23:54:13 +0530

    Configured the swPush for push notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-pwa';
+export class AppComponent implements OnInit {
+  title = 'Angular PWA';
+  constructor(private swPush: SwPush) {}
+  ngOnInit(): void {}
+
+  pushSubscription() {
+    if (!this.swPush.isEnabled) {
+      console.log('Notifications is not enabled');
+      return;
+    }
+
+    this.swPush
+      .requestSubscription({
+        serverPublicKey:
+          'BPzmREvQsHaVK5beR0D2vtyf2S_TJBhKdWlbgzSTh0Ahapu9YRmbsaauYH1bmNBHPF7HH2bO9pAfHRt5Nbwdh5s',
+      })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
   imports: [
     BrowserModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
+      enabled: true,
       registrationStrategy: 'registerWhenStable:30000'
     })
   ],
